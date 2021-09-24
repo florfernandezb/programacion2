f000bc0a59c4a48e6df3ef11181c3617a19d8bf0
ffernandezbu <florencia.bfernandez@mercadolibre.com>
2021-09-24 00:58:26 -0300

    add hover

--- views/gracias.php
+++ views/gracias.php
@@ -2,5 +2,6 @@
     <div class="gracias">
         <h2>¡Gracias por contactarnos!</h2>
         <p>Tu mensaje ha sido enviado. Te responderemos a la brevedad.</p>
+        <a href="index.php">Seguir navegando</a>
     </div>
 </main>
